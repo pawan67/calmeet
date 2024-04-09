ad14308b27bb408bec6b8bdfde15ce42b0b56667
pawan67 <pawantamada8@gmail.com>
2024-04-09 17:55:19 +0530

    added analytics

--- src/components/shared/provider.tsx
+++ src/components/shared/provider.tsx
@@ -1,7 +1,14 @@
 import { ClerkProvider } from "@clerk/nextjs";
-
+import { Analytics } from "@vercel/analytics/react";
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return (
+    <>
+      <ClerkProvider>
+        {children}
+        <Analytics />
+      </ClerkProvider>
+    </>
+  );
 };
 
 export default Providers;
