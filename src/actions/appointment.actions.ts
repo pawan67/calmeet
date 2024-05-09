26423046de977b65316d65d289a4028a961291d7
pawan67 <pawantamada8@gmail.com>
2024-05-09 13:42:01 +0530

    Refactor generateAvailableSlots function and move it to utils.ts

--- src/actions/appointment.actions.ts
+++ src/actions/appointment.actions.ts
@@ -1,7 +1,8 @@
 "use server";
-export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+export const dynamic = "force-dynamic"; // static by default, unless reading the request
 
 import { db } from "@/lib/db";
+import { generateAvailableSlots } from "@/lib/utils";
 import { AppointmentSchema } from "@prisma/client";
 
 export const availableSlots = async (
@@ -38,57 +39,3 @@ export const availableSlots = async (
     throw new Error(error.message);
   }
 };
-
-function generateAvailableSlots(
-  startOfDay: Date,
-  endOfDay: Date,
-  bookedAppointments: AppointmentSchema[]
-) {
-  const availableSlots: { 12: string; 24: string }[] = [];
-  const slotDuration = 30 * 60 * 1000; // 30 minutes in milliseconds
-  let currentTime = startOfDay.getTime();
-  const endTime = endOfDay.getTime();
-
-  while (currentTime < endTime) {
-    const slotStartTime = new Date(currentTime);
-    const slotEndTime = new Date(currentTime + slotDuration);
-
-    const isBooked = bookedAppointments.some((appointment) => {
-      const appointmentStartTime = new Date(appointment.startTime);
-      const appointmentEndTime = new Date(appointment.endTime);
-
-      return (
-        (appointmentStartTime >= slotStartTime &&
-          appointmentStartTime < slotEndTime) ||
-        (appointmentEndTime > slotStartTime &&
-          appointmentEndTime <= slotEndTime) ||
-        (appointmentStartTime < slotStartTime &&
-          appointmentEndTime > slotEndTime)
-      );
-    });
-
-    if (!isBooked) {
-      const slotStart12HourFormat = formatTime(slotStartTime, "12");
-      const slotStart24HourFormat = formatTime(slotStartTime, "24");
-      availableSlots.push({
-        12: slotStart12HourFormat,
-        24: slotStart24HourFormat,
-      });
-    }
-
-    currentTime += slotDuration;
-  }
-
-  return availableSlots;
-}
-
-function formatTime(date: Date, format: "12" | "24"): string {
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const ampm = hours >= 12 ? "pm" : "am";
-  const formattedHours = format === "12" ? ((hours + 11) % 12) + 1 : hours;
-  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-
-  return `${formattedHours}:${formattedMinutes}${format === "12" ? ampm : ""}`;
-}
-
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { AppointmentSchema } from "@prisma/client";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -24,3 +25,58 @@ export function generateScale({
 
   return Object.fromEntries(scale);
 }
+
+
+export function generateAvailableSlots(
+  startOfDay: Date,
+  endOfDay: Date,
+  bookedAppointments: AppointmentSchema[]
+) {
+  const availableSlots: { 12: string; 24: string }[] = [];
+  const slotDuration = 30 * 60 * 1000; // 30 minutes in milliseconds
+  let currentTime = startOfDay.getTime();
+  const endTime = endOfDay.getTime();
+
+  while (currentTime < endTime) {
+    const slotStartTime = new Date(currentTime);
+    const slotEndTime = new Date(currentTime + slotDuration);
+
+    const isBooked = bookedAppointments.some((appointment) => {
+      const appointmentStartTime = new Date(appointment.startTime);
+      const appointmentEndTime = new Date(appointment.endTime);
+
+      return (
+        (appointmentStartTime >= slotStartTime &&
+          appointmentStartTime < slotEndTime) ||
+        (appointmentEndTime > slotStartTime &&
+          appointmentEndTime <= slotEndTime) ||
+        (appointmentStartTime < slotStartTime &&
+          appointmentEndTime > slotEndTime)
+      );
+    });
+
+    if (!isBooked) {
+      const slotStart12HourFormat = formatTime(slotStartTime, "12");
+      const slotStart24HourFormat = formatTime(slotStartTime, "24");
+      availableSlots.push({
+        12: slotStart12HourFormat,
+        24: slotStart24HourFormat,
+      });
+    }
+
+    currentTime += slotDuration;
+  }
+
+  return availableSlots;
+}
+
+export function formatTime(date: Date, format: "12" | "24"): string {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const ampm = hours >= 12 ? "pm" : "am";
+  const formattedHours = format === "12" ? ((hours + 11) % 12) + 1 : hours;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+  return `${formattedHours}:${formattedMinutes}${format === "12" ? ampm : ""}`;
+}
+
