f9281d62d71f974a4a8ecd80caa471b8a731ff5e
pawan67 <pawantamada8@gmail.com>
2024-05-10 12:33:16 +0530

    Refactor header component and update styles in header.tsx

--- src/app/(marketing)/_components/header.tsx
+++ src/app/(marketing)/_components/header.tsx
@@ -6,31 +6,33 @@ import Link from "next/link";
 const Header = () => {
   const { isSignedIn, isLoaded, user } = useUser();
   return (
-    <header className="py-4 flex items-center justify-between">
-      <div className=" flex items-center gap-3">
-        <img className="w-10" src="/images/logo.png" alt="logo" />
-        <h1 className=" font-semibold text-xl">Calmeet</h1>
-      </div>
-      <div>
-        {isSignedIn ? (
-          <Link href="/dashboard">
-            <ShimmerButton className="shadow-2xl">
-              <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
-                Enter Calmeet
-              </span>
-            </ShimmerButton>
-          </Link>
-        ) : (
-          <SignInButton mode="modal">
-            <ShimmerButton className="shadow-2xl">
-              <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
-                Login
-              </span>
-            </ShimmerButton>
-          </SignInButton>
-        )}
-      </div>
-    </header>
+    <div className=" fixed z-[100000] top-0 inset-x-0 px-4 container">
+      <header className="   py-4 flex items-center justify-between">
+        <div className=" flex items-center gap-3">
+          <img className="w-10" src="/images/logo.png" alt="logo" />
+          <h1 className=" font-semibold text-xl">Calmeet</h1>
+        </div>
+        <div>
+          {isSignedIn ? (
+            <Link href="/dashboard">
+              <ShimmerButton className="shadow-2xl">
+                <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+                  Enter Calmeet
+                </span>
+              </ShimmerButton>
+            </Link>
+          ) : (
+            <SignInButton mode="modal">
+              <ShimmerButton className="shadow-2xl">
+                <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+                  Login
+                </span>
+              </ShimmerButton>
+            </SignInButton>
+          )}
+        </div>
+      </header>
+    </div>
   );
 };
 
